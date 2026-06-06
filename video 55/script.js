b8e163be63a867df1abf0bb5753c71ce13cf4606
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-06-06 22:45:50 +0545

    js

--- video 55/script.js
+++ video 55/script.js
@@ -8,3 +8,27 @@ console.log(typeof a, typeof b, typeof c)
 
 // const a1 =20;
 // a1 = a1+10;  NOt Allowed to change the value of Const
+
+let x = "Nikhil";
+let y = 22;
+let z = 3.55;
+
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o = {
+   name : "NIkhil",
+   "job role" : 5600
+}
+console.log(o);
+
+o.salary = "1000 crore";
+console.log(o);
+
+o.salary = "9000 crore";
+console.log(o);
+
