8a1d8c6fc885f17e1e6addd161e037307b4f4a00
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-06-06 22:24:44 +0545

    js

---
+++ video 55/script.js
@@ -0,0 +1,10 @@
+console.log("Hey I am King")
+var a=5;
+var b=6;
+var c="Nikhil";
+console.log(a+b+78)
+
+console.log(typeof a, typeof b, typeof c)
+
+// const a1 =20;
+// a1 = a1+10;  NOt Allowed to change the value of Const
