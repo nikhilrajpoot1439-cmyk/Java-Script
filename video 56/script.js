6f6ee88788c797bed26fd7b3f71c42a6b60b6254
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-06-07 22:23:41 +0545

    java condition

---
+++ video 56/script.js
@@ -0,0 +1,11 @@
+console.log("Hello I am the king of Nepal in my dream");
+let age = 17;
+let grace = 2;
+// console.log("Enter Your Age")
+
+if((age+grace)>18){
+    console.log("You Can Drive")
+}
+else{
+    console.log("You cannot able to drive")
+}
