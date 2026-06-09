7cf51cb7d46b1d673063cb9cb7fa6fff90208761
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-06-09 22:11:46 +0545

    js loop

--- video 57/index.js
+++ video 57/index.js
@@ -1,21 +1,34 @@
-console.log("I am the king of Nepal in my dream")
+// console.log("I am the king of Nepal in my dream")
 
-let a=8;
-for (let i = 0; i < 10; i++) {
-    console.log(a+i)
-    
-}
-let obj = {
-    name : "Nikhil",
-    role : "Begineer Code",
-    company: "SNAPS Tech Company"
-    
-}
-for (const key in obj) {
-    if (Object.hasOwnProperty.call(obj, key)) {
-    
-    const element = obj[key];
-    console.log(element)
-    }
-    
-}
+// let a=8;
+// for (let i = 0; i < 10; i++) {
+//     console.log(a+i)
+
+// }
+// let obj = {
+//     name : "Nikhil",
+//     role : "Begineer Code",
+//     company: "SNAPS Tech Company"
+
+// }
+// for (const key in obj) {
+
+//     const element = obj[key];
+//     console.log(key, element)  
+// }
+// for (const c of "Nikhil") {
+//     console.log(c)
+// }
+
+
+
+// let i =0;
+// while(i<6){
+//     console.log(i)
+//     i++
+// }
+let i = 10;
+do {
+    console.log(i)
+    i++
+} while (i<7);
