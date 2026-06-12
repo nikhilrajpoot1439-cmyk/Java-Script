2ea36654544f115fcd8e4fc5c5eb96161ce66d5a
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-06-12 12:08:35 +0545

    video 58

---
+++ video 58/index.js
@@ -0,0 +1,18 @@
+
+// function nice(name) {
+//     console.log("Hey " + name + " you are looking good")
+//     console.log("Hey " + name + " you are learning web dev")
+//     console.log("Hey " + name + " you don't believe in love")
+//     console.log("Hey " + name + " you are Broken")
+
+// }
+
+// nice("Nikhil")
+
+
+function sum(a , b) {
+    // console.log(a+b)
+    return a+b
+}
+result = sum(9 , 10)
+console.log("The sum of number is  : ", result)
