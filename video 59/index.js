0e6ade3dbaa71762fae9efbd8cdc4ca367cb6c3a
nikhilrajpoot <nikhilrajpoot1439@gmail.com>
2026-06-12 13:26:50 +0545

    video 60

---
+++ video 59/index.js
@@ -0,0 +1,13 @@
+/*
+Create a faulty calculator using JavaScript
+
+1. This Faulty calculator does following it takes two number as input from user
+1. It performs wrong operation as follow :
+     +.......> -
+     *.......> +
+     -.......> /
+     /.......> %
+
+It performa wrong operation 10 % of the time
+
+*/
---
+++ video 60/index.js
@@ -0,0 +1,27 @@
+console.log("This is a string tutorial")
+let a = "Nikhil"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+
+console.log(a.length)
+
+let real_name = "Nikhil"
+let friend = "Tula"
+console.log("His name is " + real_name + "and his friend name is " +friend)
+
+console.log(`His name is ${real_name} and his friend name is ${friend}`
+  )
+
+
+  let b = "Swastik"
+  console.log(b.toUpperCase())
+  console.log(b.toLowerCase())
+  console.log(b.length)
+
+  console.log(b.slice(1,4))
+  console.log(b.slice(2))
+  console.log(b.replace("as" , "77"))
